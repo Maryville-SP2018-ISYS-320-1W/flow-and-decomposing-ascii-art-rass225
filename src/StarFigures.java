91cb4cf7bd2315b3ad739a5f6316c39008872264
rass225 <31581061+rass225@users.noreply.github.com>
2018-03-24 17:22:37 +0200

    changes

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -5,12 +5,24 @@
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
-
+/*     *****
+ * 	   *****
+ *       *
+ *      * *
+ *       *
+ */
 
 public class StarFigures {
-
 	public static void main(String[] args) {
-
+		for (int star = 1; star <= 2; star++) {
+			for(int i = 1; i <= 5; i++) {
+				System.out.print("*");
+			}
+			System.out.println();		
+		}
+			
 	}
-
+		for 
 }
+//>
+//<
--- src/Tricky.java
+++ src/Tricky.java
@@ -5,13 +5,32 @@
 */
 
 // 1. Your predicted output 
- 
+ // 1. message 1
+ // 2. message 2
+ // 3. Done with message 2
+ // 4. Done with main.
 
 public class Tricky {
+	 public static void main(String[] args) {
+	        message1();
+	        message2();
+	        System.out.println("Done with main.");
+	    }
+	    
+	    public static void message1() {
+	        System.out.println("This is message1.");
+	    }
+	    
+	    public static void message2() {
+	        System.out.println("This is message2.");
+	        message1();
+	        System.out.println("Done with message 2.");
+	    }
+	}
 
-}
 
 // 3. Were you correct? Explain any differences
- 
+ //No I wasn't, because apparently it runs the message 2 first, then turns back to message one. 
+//Then declares that message 2 is done and after that with main.
  
 
